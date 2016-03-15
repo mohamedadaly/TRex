87d7a24148b992720d185a8e58cc5cee5a7be21e
Mohamed Aly <mohamedadaly@gmail.com>
2016-03-15 15:10:21 +0300

    Added Ramani code that now works to construct SL phantom.

--- samples/matlab/s004_cpu_reconstruction.m
+++ samples/matlab/s004_cpu_reconstruction.m
@@ -20,6 +20,16 @@ proj_geom = astra_create_proj_geom('parallel', 1.0, 384, ...
 % For CPU-based algorithms, a "projector" object specifies the projection
 % model used. In this case, we use the "strip" model.
 proj_id = astra_create_projector('linear', proj_geom, vol_geom);
+% assumes the volume is a vector in row-major order, and the output
+% is also in row-major order
+% to emulate projection, do the following:
+%   sino = reshape(proj_mat * reshape(P',[],1), 384, 30)';
+% which should be the same as sinogram
+proj_mat_id = astra_mex_projector('matrix', proj_id);
+proj_mat = astra_mex_matrix('get', proj_mat_id);
+
+% save for input to IRT toolbox for comparison
+save sl.mat P proj_mat sinogram vol_geom proj_geom fbp
 
 % Create a sinogram from a phantom
 P = phantom(256);
@@ -32,6 +42,9 @@ figure(1); imshow(P, []); axis image; axis off;
 figure(2); imshow(sinogram, []); axis image; axis off;
 % imshow(sinogram, []);
 
+% compare full projection matrix
+sino2 = reshape(proj_mat * reshape(P',[],1), 384, 30)';
+disp(norm(sino2 - sinogram, 'fro'));
 
 % add sinogram noise
 rng(123);
@@ -65,7 +78,7 @@ cfg.option.MaxConstraintValue = 1;
 cfg.option.ClearRayLength = 1;
 cfg.option.Alpha = 2;
 cfg.option.Lambda = 1e3;
-cfg.option.ComputeIterationMetrics = 1;
+cfg.option.ComputeIterationMetrics = 0;
 cfg.option.GTReconstructionId = P_id;
 
 % Available algorithms:
@@ -78,7 +91,7 @@ alg_id = astra_mex_algorithm('create', cfg);
 % This will have a runtime in the order of 10 seconds.
 tic;
 % astra_mex_algorithm('iterate', alg_id, 30*30);
-astra_mex_algorithm('iterate', alg_id, 10);
+astra_mex_algorithm('iterate', alg_id, 25);
 toc;
 
 % Get the result
